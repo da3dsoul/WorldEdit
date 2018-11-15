PureGero/PureAsyncWorldEdit

Added "fast mode corrections"

worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/BukkitWorld.java
@@ -438,6 +438,17 @@ public class BukkitWorld extends AbstractWorld {
     }
 
     @Override
+    public boolean notifyAndLightBlock(Vector position, com.sk89q.worldedit.world.block.BlockState previousType) throws WorldEditException {
+        BukkitImplAdapter adapter = WorldEditPlugin.getInstance().getBukkitImplAdapter();
+        if (adapter != null) {
+            adapter.notifyAndLightBlock(BukkitAdapter.adapt(getWorld(), position), previousType);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
     public BaseBlock getFullBlock(Vector position) {
         BukkitImplAdapter adapter = WorldEditPlugin.getInstance().getBukkitImplAdapter();
         if (adapter != null) {

worldedit-core/src/main/java/com/sk89q/worldedit/extent/world/FastModeExtent.java
@@ -29,9 +29,12 @@ import com.sk89q.worldedit.function.operation.Operation;
 import com.sk89q.worldedit.function.operation.RunContext;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
+import com.sk89q.worldedit.world.block.BlockTypes;
 
+import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -40,8 +43,10 @@ import java.util.Set;
 public class FastModeExtent extends AbstractDelegateExtent {
 
     private final World world;
+    private final Queue<Vector> positions = new ArrayDeque<>();
     private final Set<BlockVector2D> dirtyChunks = new HashSet<>();
     private boolean enabled = true;
+    private boolean postEditSimulation;
 
     /**
      * Create a new instance with fast mode enabled.
@@ -63,6 +68,9 @@ public class FastModeExtent extends AbstractDelegateExtent {
         checkNotNull(world);
         this.world = world;
         this.enabled = enabled;
+        if (enabled) {
+            this.postEditSimulation = true;
+        }
     }
 
     /**
@@ -83,11 +91,27 @@ public class FastModeExtent extends AbstractDelegateExtent {
         this.enabled = enabled;
     }
 
+    public boolean isPostEditSimulationEnabled() {
+        return postEditSimulation;
+    }
+
+    public void setPostEditSimulationEnabled(boolean enabled) {
+        this.postEditSimulation = enabled;
+    }
+
     @Override
     public boolean setBlock(Vector location, BlockStateHolder block) throws WorldEditException {
         if (enabled) {
             dirtyChunks.add(new BlockVector2D(location.getBlockX() >> 4, location.getBlockZ() >> 4));
-            return world.setBlock(location, block, false);
+
+            if (world.setBlock(location, block, false)) {
+                if (postEditSimulation) {
+                    positions.offer(location);
+                }
+                return true;
+            }
+
+            return false;
         } else {
             return world.setBlock(location, block, true);
         }
@@ -101,6 +125,16 @@ public class FastModeExtent extends AbstractDelegateExtent {
                 if (!dirtyChunks.isEmpty()) {
                     world.fixAfterFastMode(dirtyChunks);
                 }
+
+                if (postEditSimulation) {
+                    while (run.shouldContinue() && !positions.isEmpty()) {
+                        Vector position = positions.poll(); // Remove from queue
+                        world.notifyAndLightBlock(position, BlockTypes.AIR.getDefaultState());
+                    }
+
+                    return !positions.isEmpty() ? this : null;
+                }
+
                 return null;
             }
 

worldedit-core/src/main/java/com/sk89q/worldedit/world/NullWorld.java
@@ -64,6 +64,11 @@ public class NullWorld extends AbstractWorld {
     }
 
     @Override
+    public boolean notifyAndLightBlock(Vector position, BlockState previousType) throws WorldEditException {
+        return false;
+    }
+
+    @Override
     public int getBlockLightLevel(Vector position) {
         return 0;
     }

worldedit-core/src/main/java/com/sk89q/worldedit/world/World.java
@@ -32,6 +32,7 @@ import com.sk89q.worldedit.function.mask.Mask;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.util.Direction;
 import com.sk89q.worldedit.util.TreeGenerator;
+import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
 import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.weather.WeatherType;
@@ -93,6 +94,16 @@ public interface World extends Extent {
      * @return true if the block was successfully set (return value may not be accurate)
      */
     boolean setBlock(Vector position, BlockStateHolder block, boolean notifyAndLight) throws WorldEditException;
+
+    /**
+     * Notifies the simulation that the block at the given location has
+     * been changed and it must be re-lighted (and issue other events).
+     *
+     * @param position position of the block
+     * @param previousType the type of the previous block that was there
+     * @return true if the block was successfully notified
+     */
+    boolean notifyAndLightBlock(Vector position, BlockState previousType) throws WorldEditException;
 
     /**
      * Get the light level at the given block.

worldedit-forge/src/main/java/com/sk89q/worldedit/forge/ForgeWorld.java
@@ -213,6 +213,12 @@ public class ForgeWorld extends AbstractWorld {
         return successful;
     }
 
+    @Override
+    public boolean notifyAndLightBlock(Vector position, BlockState previousType) throws WorldEditException {
+        // TODO Implement
+        return false;
+    }
+
     // Can't get the "Object" to be right for withProperty w/o this
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private IBlockState applyProperties(BlockStateContainer stateContainer, IBlockState newState, Map<Property<?>, Object> states) {

worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeWorld.java
@@ -162,6 +162,12 @@ public abstract class SpongeWorld extends AbstractWorld {
     }
 
     @Override
+    public boolean notifyAndLightBlock(Vector position, com.sk89q.worldedit.world.block.BlockState previousType) throws WorldEditException {
+        // TODO Move this to adapter
+        return false;
+    }
+
+    @Override
     public boolean regenerate(Region region, EditSession editSession) {
         return false;
     }

da3dsoul/WorldEdit

worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/BukkitWorld.java
@@ -449,6 +449,17 @@ public class BukkitWorld extends AbstractWorld {
     }
 
     @Override
+    public boolean notifyAndLightBlock(BlockVector3 position, com.sk89q.worldedit.world.block.BlockState previousType) throws WorldEditException {
+        BukkitImplAdapter adapter = WorldEditPlugin.getInstance().getBukkitImplAdapter();
+        if (adapter != null) {
+            adapter.notifyAndLightBlock(BukkitAdapter.adapt(getWorld(), position), previousType);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
     public BaseBiome getBiome(BlockVector2 position) {
         BukkitImplAdapter adapter = WorldEditPlugin.getInstance().getBukkitImplAdapter();
         if (adapter != null) {

worldedit-core/src/main/java/com/sk89q/worldedit/extent/world/FastModeExtent.java
@@ -29,9 +29,12 @@ import com.sk89q.worldedit.math.BlockVector2;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.world.World;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
+import com.sk89q.worldedit.world.block.BlockTypes;
 
+import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 /**
@@ -40,8 +43,10 @@ import java.util.Set;
 public class FastModeExtent extends AbstractDelegateExtent {
 
     private final World world;
+    private final Queue<BlockVector3> positions = new ArrayDeque<>();
     private final Set<BlockVector2> dirtyChunks = new HashSet<>();
     private boolean enabled = true;
+    private boolean postEditSimulation;
 
     /**
      * Create a new instance with fast mode enabled.
@@ -63,6 +68,9 @@ public class FastModeExtent extends AbstractDelegateExtent {
         checkNotNull(world);
         this.world = world;
         this.enabled = enabled;
+        if (enabled) {
+            this.postEditSimulation = true;
+        }
     }
 
     /**
@@ -83,11 +91,27 @@ public class FastModeExtent extends AbstractDelegateExtent {
         this.enabled = enabled;
     }
 
+    public boolean isPostEditSimulationEnabled() {
+        return postEditSimulation;
+    }
+
+    public void setPostEditSimulationEnabled(boolean enabled) {
+        this.postEditSimulation = enabled;
+    }
+
     @Override
     public boolean setBlock(BlockVector3 location, BlockStateHolder block) throws WorldEditException {
         if (enabled) {
             dirtyChunks.add(BlockVector2.at(location.getBlockX() >> 4, location.getBlockZ() >> 4));
-            return world.setBlock(location, block, false);
+
+            if (world.setBlock(location, block, false)) {
+                if (postEditSimulation) {
+                    positions.offer(location);
+                }
+                return true;
+            }
+
+            return false;
         } else {
             return world.setBlock(location, block, true);
         }
@@ -101,6 +125,16 @@ public class FastModeExtent extends AbstractDelegateExtent {
                 if (!dirtyChunks.isEmpty()) {
                     world.fixAfterFastMode(dirtyChunks);
                 }
+
+                if (postEditSimulation) {
+                    while (run.shouldContinue() && !positions.isEmpty()) {
+                        BlockVector3 position = positions.poll(); // Remove from queue
+                        world.notifyAndLightBlock(position, BlockTypes.AIR.getDefaultState());
+                    }
+
+                    return !positions.isEmpty() ? this : null;
+                }
+
                 return null;
             }
 

worldedit-core/src/main/java/com/sk89q/worldedit/world/NullWorld.java
@@ -65,6 +65,11 @@ public class NullWorld extends AbstractWorld {
     }
 
     @Override
+    public boolean notifyAndLightBlock(BlockVector3 position, BlockState previousType) throws WorldEditException {
+        return false;
+    }
+
+    @Override
     public int getBlockLightLevel(BlockVector3 position) {
         return 0;
     }

worldedit-core/src/main/java/com/sk89q/worldedit/world/World.java
@@ -33,9 +33,12 @@ import com.sk89q.worldedit.math.Vector3;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.util.Direction;
 import com.sk89q.worldedit.util.TreeGenerator;
+import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
 import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.weather.WeatherType;
+
+import java.util.Vector;
 
 /**
  * Represents a world (dimension).
@@ -94,6 +97,16 @@ public interface World extends Extent {
      * @return true if the block was successfully set (return value may not be accurate)
      */
     boolean setBlock(BlockVector3 position, BlockStateHolder block, boolean notifyAndLight) throws WorldEditException;
+
+    /**
+     * Notifies the simulation that the block at the given location has
+     * been changed and it must be re-lighted (and issue other events).
+     *
+     * @param position position of the block
+     * @param previousType the type of the previous block that was there
+     * @return true if the block was successfully notified
+     */
+    boolean notifyAndLightBlock(BlockVector3 position, BlockState previousType) throws WorldEditException;
 
     /**
      * Get the light level at the given block.

worldedit-forge/src/main/java/com/sk89q/worldedit/forge/ForgeWorld.java
@@ -212,6 +212,12 @@ public class ForgeWorld extends AbstractWorld {
         return successful;
     }
 
+    @Override
+    public boolean notifyAndLightBlock(BlockVector3 position, BlockState previousType) throws WorldEditException {
+        // TODO Implement
+        return false;
+    }
+
     // Can't get the "Object" to be right for withProperty w/o this
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private IBlockState applyProperties(BlockStateContainer stateContainer, IBlockState newState, Map<Property<?>, Object> states) {

worldedit-sponge/src/main/java/com/sk89q/worldedit/sponge/SpongeWorld.java
@@ -41,7 +41,6 @@ import com.sk89q.worldedit.world.block.BlockStateHolder;
 import com.sk89q.worldedit.world.item.ItemTypes;
 import com.sk89q.worldedit.world.weather.WeatherType;
 import com.sk89q.worldedit.world.weather.WeatherTypes;
-
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.block.BlockSnapshot;
 import org.spongepowered.api.block.BlockState;
@@ -161,6 +160,12 @@ public abstract class SpongeWorld extends AbstractWorld {
         }
 
         return true;
+    }
+
+    @Override
+    public boolean notifyAndLightBlock(BlockVector3 position, com.sk89q.worldedit.world.block.BlockState previousType) throws WorldEditException {
+        // TODO Move this to adapter
+        return false;
     }
 
     @Override